031ad841f83e3e88c113eca8a41a66580fb93069
kapinskil <kapinski.l@gmail.com>
2019-05-03 16:58:31 +0200

    1.0

---
+++ Komis/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Komis.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Komis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochodResposytory _samochodReposytory;
+
+        public HomeController(ISamochodResposytory samochodReposytory)
+        {
+            _samochodReposytory = samochodReposytory;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Tytul = "Przegląd Samochodów";
+            var samochody = _samochodReposytory.PobierzWszystkieSamochody().OrderBy(s => s.Marka);
+
+            return View(samochody);
+        }
+    }
+}
---
+++ Komis/Models/ISamochodResposytory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Komis.Models
+{
+    public interface ISamochodResposytory
+    {
+        IEnumerable<Samochod> PobierzWszystkieSamochody();
+        Samochod PobierzSamochodoID(int samochodID);
+    }
+}
